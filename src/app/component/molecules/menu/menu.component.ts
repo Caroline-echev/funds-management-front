e883ad83c8eec62838abd18c1f84a843b5196568
Diana Carolina Echeverri Cadavid <carolina.echeverri@pragma.com.co>
2024-10-19 23:32:34 -0500

    se prepara el front para produccion

--- src/app/component/molecules/menu/menu.component.ts
+++ src/app/component/molecules/menu/menu.component.ts
@@ -1,13 +1,11 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { NotificationService } from 'src/app/services/notification.service';
+import { Component, } from '@angular/core';
 import { MENU_LABELS } from 'src/app/util/constants';
-
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss']
 })
 export class MenuComponent  {
-labels =MENU_LABELS;
+labels = MENU_LABELS;
 
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'http://3.234.223.71:8090/api/v1'
 };
